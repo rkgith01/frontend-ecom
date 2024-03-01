64937adae0b6128142f20784e0be51571b1c7a71
rkgith01 <rajkapadia34@gmail.com>
2024-03-01 12:02:10 +0530

    first commit

---
+++ app/_utils/allApi.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const apiKey = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+// console.log(apiKey)
+const apiUrl = 'http://localhost:1337/api'
+// const apiUrl = process.env.NEXT_APP_API_URL
+
+
+export const axiosClient = axios.create({
+    baseURL: apiUrl,
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+        // 'X-API-KEY': apiKey
+    }
+})
+
+
+export const paymentRequest = async (endpoint, payload) => {
+    const res = await fetch(`${apiUrl}${endpoint}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${apiKey}`,
+            // 'X-API-KEY': apiKey
+        },
+        body: JSON.stringify(payload)
+    })
+    const data = await res.json()
+    return data
+}
